78989419f5e3f1479bcb17b8c3af4ec254576ffd
Ryan Luu <ryan.luu@ryanluu.dev>
2024-07-02 09:20:45 +0000

    Improve inputs

--- src/instagram_to_discord.py
+++ src/instagram_to_discord.py
@@ -2,23 +2,23 @@ import instaloader
 import requests
 import time
 
+INSTAGRAM_PROFILE = input("Enter the Instagram account you want monitor: ")
+INSTAGRAM_USERNAME = input("Enter your Instagram username to login: ")
 WEBHOOK_URL = input("Enter the Discord webhook URL: ")
-INSTAGRAM_ACCOUNT = input("Enter the Instagram account: ")
 
 # Get instance
 L = instaloader.Instaloader()
-
-L.load_session_from_file("raenlua", "./session-raenlua")
+L.load_session_from_file(INSTAGRAM_PROFILE, "./session-*")
 
 def send_to_discord(post_url):
     """Send the Instagram post URL to Discord."""
-    data = {"content": f"New post from {INSTAGRAM_ACCOUNT}: {post_url}"}
+    data = {"content": f"New post from {INSTAGRAM_PROFILE}: {post_url}"}
     response = requests.post(WEBHOOK_URL, json=data)
     print("Notification sent to Discord:", response.status_code)
 
 def check_for_new_posts():
     """Check for new posts and notify on Discord."""
-    profile = instaloader.Profile.from_username(L.context, INSTAGRAM_ACCOUNT)
+    profile = instaloader.Profile.from_username(L.context, INSTAGRAM_PROFILE)
     for post in profile.get_posts():
         # Assuming you're running this script regularly, check the latest post
         send_to_discord("https://instagram.com/p/" + post.shortcode)
