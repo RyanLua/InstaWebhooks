ba35da5e98c03b4668936356fc9a0d891858608e
Ryan Luu <ryan.luu@ryanluu.dev>
2024-07-02 09:27:28 +0000

    Add login information

--- src/instagram_to_discord.py
+++ src/instagram_to_discord.py
@@ -1,14 +1,23 @@
+"""
+Get new Instagram posts from any Instagram profile and send them to Discord using webhooks.
+
+Make sure that you have everything in the requirements.txt file installed and that you are logged into Instaloader.
+
+Use the following to login:
+    instaloader -l USERNAME
+
+If the login fails, you may need to use import_firefox_session.py to get your session file and login using that.
+"""
+
 import instaloader
 import requests
 import time
 
 INSTAGRAM_PROFILE = input("Enter the Instagram account you want monitor: ")
-INSTAGRAM_USERNAME = input("Enter your Instagram username to login: ")
 WEBHOOK_URL = input("Enter the Discord webhook URL: ")
 
 # Get instance
 L = instaloader.Instaloader()
-L.load_session_from_file(INSTAGRAM_PROFILE, "./session-*")
 
 def send_to_discord(post_url):
     """Send the Instagram post URL to Discord."""
