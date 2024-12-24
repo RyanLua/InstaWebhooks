43f3dade32a3322e41c6c24403cfc20ab8ae622d
Ryan Luu <ryan.luu@ryanluu.dev>
2024-12-24 10:20:04 +0000

    Move parsing logic into another file

--- src/instawebhooks/__main__.py
+++ src/instawebhooks/__main__.py
@@ -6,7 +6,6 @@ import io
 import logging
 import re
 import sys
-from argparse import ArgumentParser
 from typing import Dict
 from datetime import datetime, timedelta
 from itertools import dropwhile, takewhile
@@ -22,6 +21,8 @@ except ModuleNotFoundError as exc:
     raise SystemExit(
         f"{exc.name} not found.\n  pip install [--user] {exc.name}"
     ) from exc
+
+from .parser import parser
 
 
 def regex(pattern: str):
@@ -45,50 +46,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# Parse command line arguments
-parser = ArgumentParser(
-    prog="instawebhooks",
-    description=(
-        "Monitor Instagram accounts for new posts and send them to a Discord webhook"
-    ),
-    epilog="https://github.com/RaenLua/InstaWebhooks",
-)
-group = parser.add_mutually_exclusive_group()
-parser.add_argument(
-    "instagram_username",
-    help="the Instagram username to monitor for new posts",
-    type=regex(r"^[a-zA-Z_](?!.*?\.{2})[\w.]{1,28}[\w]$"),
-)
-parser.add_argument(
-    "discord_webhook_url",
-    help="the Discord webhook URL to send new posts to",
-    type=regex(
-        r"^.*(discord|discordapp)\.com\/api\/webhooks\/([\d]+)\/([a-zA-Z0-9_.-]*)$"
-    ),
-)
-group.add_argument("-q", "--quiet", help="hide all logging", action="store_true")
-group.add_argument("-v", "--verbose", help="show debug logging", action="store_true")
-parser.add_argument(
-    "-i",
-    "--refresh-interval",
-    help="time in seconds to wait before checking for new posts again",
-    type=int,
-    default=3600,
-)
-parser.add_argument(
-    "-c",
-    "--message-content",
-    help="the message content to send with the webhook",
-    type=str,
-    default="",
-)
-parser.add_argument(
-    "-e",
-    "--no-embed",
-    help="don't show the post embed and only send message content",
-    action="store_true",
-)
-parser.add_argument("--version", action="version", version="%(prog)s " + version)
 args = parser.parse_args()
 
 # Set the logger to debug if verbose is enabled
